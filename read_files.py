da4272a540450dcf9cad685d885eb2b684ab7a6f
Jupiter-Plantagenet <61136165+Jupiter-Plantagenet@users.noreply.github.com>
2022-05-30 21:04:42 +0100

    Update read_files.py

--- read_files.py
+++ read_files.py
@@ -1,1 +1,32 @@
-print("thunder")
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here
+    with open(filename) as file:
+        return file.read()
+
+
+def count_words():
+    text = read_file_content("story.txt")
+    # [assignment] Add your code here
+    split_text = text.split()
+    text_freq = dict()
+
+    for word in split_text:
+        if word in text_freq:
+            text_freq[word] = text_freq[word] + 1
+        else:
+            text_freq[word] = 1
+    
+    return text_freq
+
+
+#test code
+print(count_words())
+
+#@George Akor
+#theStormGod
+
