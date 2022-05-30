da8c3626c967be724ec7dadbbe37133203998615
Jupiter-Plantagenet <61136165+Jupiter-Plantagenet@users.noreply.github.com>
2022-05-30 21:47:53 +0100

    Update find_anagrams_main.py

--- find_anagrams_main.py
+++ find_anagrams_main.py
@@ -1,1 +1,15 @@
-print("Anagrams")
+
+def find_anagrams(ana_one, ana_two):
+
+    if sorted(ana_one) == sorted(ana_two):
+        return True
+    else:
+        return False
+
+
+#test code
+print(find_anagrams("listen", "silent"))
+
+#@George Akor
+#theStormGod
+
